2afb63e19cfde182331913996a03a2f39c50651e
RPMorganomous <mba.rick@gmail.com>
2021-04-29 17:45:42 -0500

    added x boundry control

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -38,5 +38,22 @@ public class Player : MonoBehaviour
             transform.position
                 = new Vector3(transform.position.x, 0, 0);
         }
+        else if (transform.position.y < -3.8f )
+        {
+            transform.position
+                = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        if (transform.position.x > 11)
+        {
+            transform.position
+                = new Vector3(-11, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11)
+        {
+            transform.position
+                = new Vector3(11, transform.position.y, 0);
+        }
+
     }
 }
