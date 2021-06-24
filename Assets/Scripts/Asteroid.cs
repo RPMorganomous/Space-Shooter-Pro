4ff1a62fa1a4a4b26d110daa2ec223ec52a5cf9d
RPMorganomous <mba.rick@gmail.com>
2021-06-24 08:07:36 -0500

    asteroid explodes when hit with laser

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -6,16 +6,38 @@ public class Asteroid : MonoBehaviour
 {
     [SerializeField]
     private float _rotateSpeed = 3.0f;
+    [SerializeField]
+    private GameObject _explosionPreFab;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0, 0, 10) * _rotateSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.forward * _rotateSpeed * Time.deltaTime);
     }
+
+    //check for LASER collision (trigger)
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Laser")
+        {
+            //instantiate explosion at the position of the asteroid
+
+            Instantiate(_explosionPreFab, transform.position, Quaternion.identity);
+
+            //destroy explosion after 3 seconds
+            Destroy(other.gameObject);
+            Destroy(this.gameObject, 0.25f);
+        }
+    }
+
+
 }
